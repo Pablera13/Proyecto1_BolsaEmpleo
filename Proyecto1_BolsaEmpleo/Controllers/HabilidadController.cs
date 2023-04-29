52acc257983c478778ebecf85d039b13ba0d520d
JOSE FONSECA MENDEZ <jose.fonseca.mendez@est.una.ac.cr>
2023-04-29 12:29:13 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto1_BolsaEmpleo/Controllers/HabilidadController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto1_BolsaEmpleo.Data;
+using Proyecto1_BolsaEmpleo.Models;
+using Proyecto1_BolsaEmpleo.RequestObjects;
+
+namespace Proyecto1_BolsaEmpleo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class HabilidadController : ControllerBase
+    {
+
+        private readonly MyApiContext _context;
+
+        public HabilidadController(MyApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Habilidad>> PostHabilidad(HabilidadVm habilidadRequest)
+        {
+            if (_context.Habilidad == null)
+            {
+                return Problem("Entity set 'MyApiContext.Habilidad'  is null.");
+            }
+
+            Habilidad newHabilidad = new Habilidad();
+            newHabilidad.Nombre = habilidadRequest.Nombre;
+         
+            _context.Habilidad.Add(newHabilidad);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostHabilidad", new { id = newHabilidad.Id });
+        }
+
+
+
+    }
+}
---
+++ Proyecto1_BolsaEmpleo/RequestObjects/HabilidadVm.cs
@@ -0,0 +1,11 @@
+﻿using Proyecto1_BolsaEmpleo.Models;
+
+namespace Proyecto1_BolsaEmpleo.RequestObjects
+{
+    public class HabilidadVm
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
