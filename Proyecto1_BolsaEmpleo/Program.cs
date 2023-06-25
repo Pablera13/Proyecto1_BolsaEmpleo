e1be429e4099f26b2a6a82740d53d87e9be9b513
StevenCA-096 <115894511+StevenCA-096@users.noreply.github.com>
2023-06-25 07:56:25 -0600

    Update Program.cs allow methods and origins

--- Proyecto1_BolsaEmpleo/Program.cs
+++ Proyecto1_BolsaEmpleo/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.AllowAnyOrigin().WithMethods().WithHeaders();
+                          policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
                       });
 });
 
